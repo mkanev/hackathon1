1ffc425eb4bae21df34b4e65f6f5bc553de911f9
Maksim Kanev <maksim.kanev@gmail.com>
2013-10-14 13:11:47 +0400

    Init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('hackathon20131014App')
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma',
+            'Twitter Bootstrap'
+        ];
+    });
